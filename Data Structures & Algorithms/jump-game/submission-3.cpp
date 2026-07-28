0affadb25653e2df72cc28a7edc94101e03434c5
deepakk789 <deepak080105@gmail.com>
2026-07-28 16:26:20 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool solvewithdp(int ind,vector<int>& nums,vector<int>& dp){
+        if(ind==0) return true;
+        if(dp[ind]!=-1) return dp[ind];
+        for(int i=ind-1;i>=0;i--){
+            if(nums[i]>(ind-1)-i && solvewithdp(i,nums,dp)){
+                return dp[i]=true;
+            }
+        }
+        return dp[ind]=false;
+    }
+    bool solve(int ind,vector<int>& nums){
+        if(ind==0) return true;
+        for(int i=ind-1;i>=0;i--){
+            if(nums[i]>(ind-1)-i && solve(i,nums)){
+                cout<<i<<endl;
+                cout<<ind<<'h'<<endl;
+                return true;
+            }
+        }
+        return false;
+    }
+    bool canJump(vector<int>& nums) {
+        // tc o(n*n) and sc o(n)
+        // int n=nums.size();
+        // return solve(n-1,nums);
+
+        // tc o(n) and sc o(n+n)
+        // int n=nums.size();
+        // vector<int> dp(n,-1);
+        // return solvewithdp(n-1,nums,dp);
+
+
+        // tc o(n) and sc o(1)
+        int n=nums.size();
+        int reach=0;
+        for(int i=0;i<n;i++){
+            if(reach>=i) reach=max(reach,nums[i]+i);
+            else  break;
+        }
+        return (reach>=n-1)? true:false;
+
+    }
+};
